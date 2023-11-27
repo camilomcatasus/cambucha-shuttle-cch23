b74d56739d2e6a592e870bee07164e4f1adf1afb
Camilo Catasus <camilomcatasus@gmail.com>
2023-11-27 12:27:53 -0500

    Bonus challenge

--- src/main.rs
+++ src/main.rs
@@ -6,10 +6,15 @@ async fn index() -> HttpResponse {
     return HttpResponse::Ok().finish();
 }
 
+#[get("/-1/error")]
+async fn fake_error() -> HttpResponse {
+    return HttpResponse::InternalServerError().finish();
+}
+
 #[shuttle_runtime::main]
 async fn main() -> ShuttleActixWeb<impl FnOnce(&mut ServiceConfig) + Send + Clone + 'static> {
     let config = move |cfg: &mut ServiceConfig| {
-        cfg.service(index);
+        cfg.service(index).service(fake_error);
     };
 
     Ok(config.into())
